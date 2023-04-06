367ff36b28dd04dd2a6e970ddb8a16e07f19c7a2
CSWellesSun <3200102339@zju.edu.cn>
2023-04-06 13:32:56 +0800

    client starter code

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
 import yariga from "./yariga.svg";
-import martech3 from "./martech3.svg";
 
-export { logo, yariga, martech3 };
+export { logo, yariga };
---
+++ client/src/contexts/index.tsx
@@ -0,0 +1,54 @@
+import React, {
+    PropsWithChildren,
+    createContext,
+    useEffect,
+    useState,
+} from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { DarkTheme, LightTheme } from "@refinedev/mui";
+
+type ColorModeContextType = {
+    mode: string;
+    setMode: () => void;
+};
+
+export const ColorModeContext = createContext<ColorModeContextType>(
+    {} as ColorModeContextType,
+);
+
+export function ColorModeContextProvider(props: {children: React.ReactNode}) {
+    const colorModeFromLocalStorage = localStorage.getItem("colorMode");
+    const isSystemPreferenceDark = window?.matchMedia(
+        "(prefers-color-scheme: dark)",
+    ).matches;
+
+    const systemPreference = isSystemPreferenceDark ? "dark" : "light";
+    const [mode, setMode] = useState(
+        colorModeFromLocalStorage || systemPreference,
+    );
+
+    useEffect(() => {
+        window.localStorage.setItem("colorMode", mode);
+    }, [mode]);
+
+    const setColorMode = () => {
+        if (mode === "light") {
+            setMode("dark");
+        } else {
+            setMode("light");
+        }
+    };
+
+    return (
+        <ColorModeContext.Provider
+            value={{
+                setMode: setColorMode,
+                mode,
+            }}
+        >
+            <ThemeProvider theme={mode === "light" ? LightTheme : DarkTheme}>
+                {props.children}
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+};
